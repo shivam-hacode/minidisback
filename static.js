5ff3975e751d32f6aa505e820436f89fd1c6a1b4
Shivam Sharma <89838997+shivam-hacode@users.noreply.github.com>
2026-04-18 01:03:13 +0530

    Update static.js

--- static.js
+++ static.js
@@ -1,5 +1,5 @@
 // export const HOST = 'http://192.168.1.2:5000/api';
 
-export const HOST = 'https://16.170.145.126/api';
+export const HOST = 'https://api.mdresult.com/api';
 
 // export const HOST = 'http://192.168.1.4:5000/api';
